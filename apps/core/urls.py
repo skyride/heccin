684754b24def084dce372d41604f594df67472a7
Adam Findlay <adam.findlay@labela.nl>
2020-09-17 04:22:57 +0000

    More types

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -4,5 +4,6 @@ from .views import name_view
 
 
 urlpatterns = [
-    path("", name_view)
+    path("", name_view),
+    path("<str:flag>", name_view)
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,16 +1,29 @@
+from django.http import Http404
 from django.http.request import HttpRequest
 from django.shortcuts import render
 
 from .utils import get_name_from_host
 
 
-def name_view(request: HttpRequest):
-    name = get_name_from_host(request.get_host())
+FLAG_STYLE_MAP = {
+    "pride": "pride-rainbow",
+    "nb": "pride-nb",
+    "lesbian": "pride-lesbian-v2"
+}
+
+
+def name_view(request: HttpRequest, flag: str = "pride"):
+    # Check the flag is valid
+    if flag not in FLAG_STYLE_MAP:
+        raise Http404("We don't know that flag :(")
+
+    name = get_name_from_host(request.get_host())    
+    state = "gay" if flag in ["pride", "lesbian"] else "valid"
 
     return render(
         request,
         "core/name.html",
         context={
             "name": name,
-            "state": "gay",
-            "style": "pride-rainbow"})
+            "state": state,
+            "style": FLAG_STYLE_MAP[flag]})
